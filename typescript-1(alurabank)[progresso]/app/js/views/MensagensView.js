88ecc208de1b5642e5698dfa4c63770d5cceac03
lucio <luciocarvalhodev@gmail.com>
2021-02-13 16:43:26 -0300

    typescript-1 concluido

--- typescript-1(alurabank)[progresso]/app/js/views/MensagensView.js
+++
@@ -1,7 +0,0 @@
-class MensagensView extends View {
-    template(texto) {
-        return `
-            <p class="message-text">${texto}</p>
-        `;
-    }
-}
--- typescript-1(alurabank)[progresso]/app/js/views/View.js
+++
@@ -1,8 +0,0 @@
-class View {
-    constructor(seletor) {
-        this._target = document.querySelector(seletor);
-    }
-    update(model) {
-        this._target.innerHTML = this.template(model);
-    }
-}
