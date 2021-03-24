ec86698134c8612f03a8a78d496c98b9d88da641
lucio <luciocarvalhodev@gmail.com>
2021-03-24 14:53:29 -0300

    end

--- react-router[progresso]/src/components/Cabecalho.jsx
+++ react-router[progresso]/src/components/Cabecalho.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import imagem from './../assets/img/doguito.svg';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import imagem from '../assets/img/doguito.svg'
+import '../assets/css/componentes/cabecalho.css'
 
-import './../assets/css/componentes/cabecalho.css';
+const Cabecalho = () => { 
+  return(
+    <header className="cabecalho container">
+      <div className="menu-hamburger">
+        <span className="menu-hamburger__icone">
+        </span>
+      </div>
+      <div className="cabecalho-container">
+        <Link to="/" className="flex flex--centro">
+          <img className="cabecalho__logo" src={imagem} alt="Logo Doguito"/>
+          <h1 className="cabecalho__titulo">PetShop</h1>
+        </Link>
+      </div>
 
-function Cabecalho() {
+      <nav className="menu-cabecalho">
+        <ul className="menu-itens">
+          <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+          <li><Link to="#" className="menu-item">Produtos</Link></li>
+          <li><Link to="/" className="menu-item">Blog</Link></li>
+          <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
+        </ul>
 
-    return (
-        <header className="cabecalho container">
-            <div className="menu-hamburguer">
-                <span className="menu-hambuguer__icone">                    
-                </span>
-            </div>
-            <div className="cabecalho-container">
-                <a href="/" className="flex flex--centro">
-                    <img className="cabecalho__logo" src={imagem} alt="Logo Doguito"/>
-                    <h1 className="cabecalho__titulo">Petshop</h1>
-                </a>
-            </div>
-            <nav className="menu-cabecalho">
-                <ul className="menu-items">
-                    <li><a href="/" className="menu-item menu-item--entrar">Entrar</a></li>
-                    <li><a href="/" className="menu-item menu-item">Produtos</a></li>
-                    <li><a href="/" className="menu-item menu-item">Blog</a></li>
-                    <li><a href="/sobre" className="menu-item menu-item">Sobre</a></li>
-                </ul>
-            </nav>
-            <div className="menu-cabecalho"></div>
-        </header>
-    );
+      </nav>
+      <div className="menu-cabecalho-background"></div>
+    </header>
+  )
 }
 
-export default Cabecalho;
+
+export default Cabecalho
